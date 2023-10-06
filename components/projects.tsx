ccfbb641fe9652dbb24b31c4a69eddcae4f9c31d
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-06 02:09:42 -0600

    feat: consume activeSection + setter in existing sections - extracting activeSection + setActiveSection from context provider - using inView and useEffect in existing sections to trigger state change in context provider - specified threshold for inView so activeSection isn't immediately set when less than 50% of the section is in view

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import SectionHeading from "@/components/section-header";
 import { projectsData } from "@/lib/data";
 import Project from "@/components/project";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Projects() {
+  const { ref, inView } = useInView({ threshold: 0.5 });
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("Projects");
+    }
+  }, [inView, setActiveSection]);
   return (
-    <section id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28">
       <SectionHeading>Projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
