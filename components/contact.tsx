58ed59ea18a4f9a94d792c95cf015661ca6542a2
Helen Gezahegn <gezahegn@ualberta.ca>
2023-10-06 20:13:58 -0600

    feat: use react-email to style email + add toaster to catch submit button response - styled email with react-email and react-email/tailwind in contact-form-email.tsx - added pending animation on submit button by using experimental useFormStatus - added toasters to display error or success messages - refactored submit button for pending logic + animation

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,10 +2,11 @@
 
 import React from "react";
 import SectionHeader from "./section-header";
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "@/components/submit-btn";
+import toast from "react-hot-toast";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -37,7 +38,14 @@ export default function Contact() {
           // console.log("Running on client");
           // console.log(formData.get("senderEmail"));
           // console.log(formData.get("emailMessage"));
-          await sendEmail(formData);
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
         }}
       >
         <input
@@ -55,13 +63,7 @@ export default function Contact() {
           required
           maxLength={5000}
         />
-        <button
-          type="submit"
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
-        >
-          Submit
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
