b3b8a2448a8ce7942f9000be0e411421241f00c0
Helen Gezahegn <gezahegn@ualberta.ca>
2023-11-08 02:48:35 -0700

    chore: update about me text

--- components/about.tsx
+++ components/about.tsx
@@ -20,31 +20,20 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
       <p className="mb-3">
         {`With a love for learning and a passion for
-        crafting fast and elegant applications, I believe that the art of coding
-        is a journey, and I'm always excited to embark on new adventures.`}
+        crafting fast and elegant applications, I thrive on creating solutions that have a real-world impact.`}
       </p>
       <p className="mb-3">
-        {`My work isn't just about lines of code; it's about creating solutions that
-        have a real-world impact. I thrive on solving complex problems and
-        turning ideas into practical applications that can change lives. Regardless if
-        it's streamlining processes, enhancing user experiences, or addressing
-        societal challenges, I'm always eager to roll up my sleeves and make a
-        difference.`}
-      </p>
-      <p className="mb-3">
-        {`I firmly believe in the power of technology to drive positive change.
-        Whether it's building software to help nonprofits reach their goals,
-        creating platforms to connect people in meaningful ways, or using data
-        to address pressing global issues, I'm dedicated to using my skills to
-        contribute to a better world.`}
+        {`I firmly believe in the power of technology to drive positive change, 
+        whether it's streamlining processes, enhancing user experiences, 
+        building software to help nonprofits reach their goals, creating platforms 
+        to connect people in meaningful ways, or using data to address pressing global issues. 
+        I'm always eager to roll up my sleeves and make a difference. `}
       </p>
 
       <p className="mb-3">
         {` When I'm not immersed in the world of
         code, you'll find me travelling (on country 30), trying to
-        learn a new language (currently working on my French), or trying something new 
-        with my family and friends. I believe that a balanced life
-        leads to better creativity and innovation.`}
+        learn a new language (currently working on my French), or chillin' with the fam.`}
       </p>
     </motion.section>
   );
